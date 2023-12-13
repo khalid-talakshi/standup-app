7f2bccddbadc6f4921b9a4f1b4989a9fcb0b4e5b
Khalid Talakshi <khalid.talakshi@bluejays.com>
2023-12-13 14:14:47 -0500

    clean up error

--- app/error.ts
+++ app/error.ts
@@ -8,23 +8,6 @@ export const constructError = (code: number): Error => {
   switch (code) {
     case 1:
       return { error: "Invalid email or password" };
-    case 2:
-      return { error: "Missing Email" };
-    case 3:
-      return { error: "Missing Password" };
-    case 4:
-      return { error: "Missing First Name" };
-    case 5:
-      return { error: "Missing Last Name" };
-    case 6:
-      return { error: "Missing Password Confirm" };
-    case 7:
-      return { error: "User with this email already exists" };
-    case 8:
-      return { error: "Passwords do not match" };
-    case 9:
-      return { error: "Invalid Email" };
-
     default:
       return { error: "Unknown error" };
   }
